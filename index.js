93c58a0ab71ed00a1a7f0ca1ffd0f80d550bfd1d
Luis Azcuaga <37643667+luisAzcuaga@users.noreply.github.com>
2026-07-28 20:36:57 -0600

    chore: simplify combined rune logic

--- index.js
+++ index.js
@@ -59,25 +59,25 @@ function setupRunesListeners() {
       document.getElementById('combined-runes')
         .appendChild(clonedRune);
 
-      // does the selected rune has a center segment? if so, let's shorten it.
-      if (clonedRune.querySelector('.rune-middle-chunk-container .rune-segment.middle-center')) {
-        clonedRune.querySelector('.rune-middle-chunk-container .rune-segment.middle-center').style.height = '16.666px';
+      // in this next section we shorten middle left or middle center segments.
+      // We always skip middle-right because is never usen by any rune.
+      if (clonedRune.querySelector('[id^=consonant-] .rune-segment.middle-center')) {
+        // The selected rune is a consonant with a middle-center segment? if so, let's shorten it.
+        clonedRune.querySelector('[id^=consonant-] .rune-segment.middle-center').style.height = '16.666px';
       }
 
-      // does the selectred rune has a left segment? if so, let's split it in two, so it's looks striken through.
-      const leftSegment = clonedRune.querySelector('.rune-middle-chunk-container .rune-segment.middle-left');
-      if (leftSegment) {
-        const clonedLeft = leftSegment.cloneNode(false);
-        leftSegment.style.height = '33.3px';
-        clonedLeft.style.height = '16.666px';
-        clonedLeft.style.top = '100%';
-        clonedRune.querySelector('.rune-middle-chunk-container')
-          .appendChild(clonedLeft);
+      // The selectred rune is a vowel with a middle-left segment? if so, let's split it in two, so it's looks striken through.
+      const middleLeftSegment = clonedRune.querySelector('[id^=vowel-] .rune-segment.middle-left');
+      if (middleLeftSegment) {
+        const middleLeftClonnedSegment = middleLeftSegment.cloneNode(false);
+        middleLeftSegment.style.height = '33.3px';
+
+        middleLeftClonnedSegment.style.height = '16.666px';
+        middleLeftClonnedSegment.style.top = '100%';
+        middleLeftSegment.parentElement.appendChild(middleLeftClonnedSegment);
       }
-      if (clonedRune.querySelector('.rune-middle-chunk-container .rune-segment.middle-right')) {
-        clonedRune.querySelector('.rune-middle-chunk-container .rune-segment.middle-right').style.height = '16.666px';
-      }
-      // add this element to .rune-middle-chunk-container
+
+      // This block adds the horizontal strike through segment.
       const horizontalRuler = `<span class="rune-segment middle-center" style="height: 1px !important; width: 50px; left: 0; bottom: 50%;"></span>`;
       clonedRune.querySelector('.rune-middle-chunk-container').insertAdjacentHTML('beforeend', horizontalRuler);
     })
